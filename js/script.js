fc0986f13dc6442ccfb121b89777c8bd6ea7061d
LuisJann <giannoneluigi10@gmail.com>
2022-10-31 18:42:44 +0100

    Snack 1.B completo

--- js/script.js
+++ js/script.js
@@ -34,3 +34,48 @@ function findAnimal() {
 
 findAnimal();
 console.log(newAnimalArray, "fuori function");
+
+
+/*2. Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età. */
+
+const people = [
+    {
+        name: "Luigi",
+        lastName: "Giannone",
+        age: 26
+    },
+    {
+        name: "Francesca",
+        lastName: "Rossi",
+        age: 35
+    },
+    {
+        name: "Michele",
+        lastName: "Bianchi",
+        age: 72
+    },
+    {
+        name: "Carmela",
+        lastName: "Verdi",
+        age: 84
+    },
+];
+
+const readyToDrive = [];
+
+function checkAge() {
+    for(let i = 0; i < people.length; i++){
+        thisDriver = people[i];
+        console.log(thisDriver);
+        if(thisDriver.age < 70){
+            readyToDrive.push(thisDriver)
+        }
+    }
+    return thisDriver
+}
+
+checkAge();
+console.log(readyToDrive);
